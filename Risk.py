bae8f4528be380c61088f263a9afee6ce8dffe1d
Kevin Malby <kevin.malby@gmail.com>
2013-11-20 20:34:04 -0800

    Modified the human move to respond in way computer would.

--- Risk.py
+++ Risk.py
@@ -1,12 +1,19 @@
 import random
+<<<<<<< Updated upstream
 import sys
 import copy
+=======
+import sys, random
+import globalVals
+from risk_player import RiskPlayer
+>>>>>>> Stashed changes
 
 class Risk:
 
-    def __init__(self,country_file,num_players):
+    def __init__(self,country_file,card_file,num_players):
         self.countries = {}  # {"North America:([South America,],{player:number_armies})
         self.map = {} # {"North America":["Eastern United States", "Greenland"]}
+<<<<<<< Updated upstream
         self.players = [0 for x in range(num_players)]
         self.makeMap(country_file)
 
@@ -14,6 +21,15 @@ class Risk:
     # Read in the countries
     ############################
     def makeMap(self, country_file):
+=======
+        self.territoryCards = {} # {"North America":"Canon"}
+        self.tradeInValues = (4,6,8,10,12,15,20,25,30,35,40,45)
+        self.tradeInPlaceholder = 0
+        self.players = []
+        self.playersMove = -1
+
+        # Build the map and countries structures
+>>>>>>> Stashed changes
         with open(country_file, 'r') as input:
             line = input.readline()
             while line != "":
@@ -25,8 +41,20 @@ class Risk:
                         line = line.split("-")
                         edges = line[1].split(',') # country- border_1, border_2 
                         self.map[cty[0]].append(line[0].strip())
-                        self.countries[line[0]] =([x.strip() for x in edges],{-1:0}) 
+                        self.countries[line[0]] =([x.strip() for x in edges],[-1,0]) 
                 line = input.readline()
+
+        # Build the territory cards structure
+        with open(card_file, 'r') as input:
+            line = input.readline()
+            while line != "":
+                countrySymbol = line.split('-')
+                countrySymbol[1] = countrySymbol[1].strip()
+                self.territoryCards[countrySymbol[0]] = countrySymbol[1]
+                line = input.readline()
+            self.territoryCards['Wild1'] = 'wild'
+            self.territoryCards['Wild2'] = 'wild'
+
                  
         print self.map
         print "\n\n"
@@ -51,6 +79,7 @@ class Risk:
     def Clone(self):
        return copy.deepcopy(self.countries)
     
+<<<<<<< Updated upstream
     def DoMove(self, player, stage, move):
         pass
     
@@ -65,6 +94,19 @@ class Risk:
     #        
     ###############################################
     def GetMoves(self, player, stage, num_armies=0):
+=======
+    def DoMove(self, move, player, phase):
+        for country, armies in move:
+            updatePlayer = armies[0]
+            self.countries[country][1][updatePlayer] += armies[1]
+
+            if self.countries[country][1][updatePlayer] == 0:
+                removePlayer = self.countries[country][updatePlayer]
+                del removePlayer[removePlayer.keys()[1]]
+                del player.occupiedCountries[country]
+        
+    def GetMoves(self, player ):
+>>>>>>> Stashed changes
         moves = []
         # Placing Armies
         if stage == 1:
@@ -90,10 +132,87 @@ class Risk:
     def DoHumanMove(self):
         pass
     def __repr__(self):
-        pass
+
+        gameState = ''
+        for continent in self.map:
+            gameState += '\n' + continent + '\n'
+
+            for country in self.map[continent]:
+
+                curCountry = self.countries[country]
+                playerArmies = curCountry[1]
+                gameState += country + ': [ ' + str(playerArmies) + ' ]\n'
+
+        return gameState
+        
     def CheckFullBoard(self):
         pass
 
     def CheckEndingConditions(self, playerJustMoved):
         pass
-    
+
+
+    def setContinentControl(self):
+
+        for player in self.players:
+
+            # Reset the contents of the continentsHeld dictionary
+            player.continentsHeld = {}
+
+            # Check if player controls each of the continents
+            continentCount = {'North America':0, 'South America':0, 'Europe':0, 'Australia':0, 'Asia':0, 'Africa':0}
+
+            for country in player.occupiedCountries:
+                
+                for continent in self.map:
+                    if country in continent:
+                        continentCount[continent] += 1
+
+            if continentCount['North America'] == 9:
+                player.continentsHeld['NA'] = 5
+
+            if continentCount['Europe'] == 7:
+                player.continentsHeld['EU'] = 5
+
+            if continentCount['Australia'] == 4:
+                player.continentsHeld['AU'] = 2
+
+            if continentCount['South America'] == 4:
+                player.continentsHeld['SA'] = 2
+
+            if continentCount['Asia'] == 12:
+                player.continentsHeld['AS'] = 7
+
+            if continentCount['Africa'] == 6:
+                player.continentsHeld['AF'] = 3
+
+    def randomizeInitialState(self):
+        armiesToPlaceEach = 40-((globalVals.maxPlayers-2)*5)
+
+        self.players.append(RiskPlayer(0, 'Red'))
+        self.players.append(RiskPlayer(1, 'Blue'))
+
+
+        numEmptyCountries = len(self.countries)
+        while armiesToPlaceEach > 0:
+
+            for i in range(globalVals.maxPlayers):
+
+                if numEmptyCountries > 0:
+                    while True:
+                        country = random.choice(self.countries.keys())
+
+                        if self.countries[country][1][0] == -1:
+                            self.countries[country][1][0] = self.players[i].playerNum
+                            self.countries[country][1][1]  = 1
+                            self.players[i].occupiedCountries[country] = 1
+                            numEmptyCountries -= 1
+                            break
+
+                else:
+                    country = random.choice(self.players[i].occupiedCountries.keys())
+                    self.countries[country][1][1] += 1
+                    self.players[i].occupiedCountries[country] += 1
+
+            armiesToPlaceEach -= 1
+
